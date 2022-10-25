dddb87fbb40148125dcd1d32e7e46efd0feb9d0b
Rakibul Hasan <39860864+rakibhasantusar@users.noreply.github.com>
2022-10-25 15:26:54 +0600

    firebase setup, react router setup, folder structure, ready for work

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,41 @@
+
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+                loader: () => fetch('')
+            },
+            {
+                path: '/category/:id',
+                element: <Category></Category>,
+                loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`)
+            },
+            {
+                path: '/news/:id',
+                element: <PrivateRoute><News></News></PrivateRoute>,
+                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register />
+            },
+            {
+                path: '/terms',
+                element: <TermsAndCondition></TermsAndCondition>
+            },
+            {
+                path: '/profile',
+                element: <PrivateRoute><Profile></Profile></PrivateRoute>
+            }
+        ]
+    }
+])
